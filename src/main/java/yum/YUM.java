4263108ab6edcc5422f639e9005f8663e8c055ef
hm <hmkuak@naver.com>
2016-05-05 14:16:19 +0900

    Release 1.0.4

--- src/main/java/yum/YUM.java
+++ src/main/java/yum/YUM.java
@@ -305,32 +305,32 @@ public class YUM {
 				File files = new File(this.path);
 
 				File matchFile = null;
-				if (files.isDirectory()) {
-					for (File file : files.listFiles()) {
-						if (!file.getName().toLowerCase().contains(".jar"))
+
+				if (!files.isDirectory())
+					return;
+
+				for (File file : files.listFiles()) {
+					if (!file.getName().toLowerCase().contains(".jar"))
+						continue;
+
+					PluginDescription description = null;
+					try {
+						JarFile jar = new JarFile(file);
+						JarEntry entry = jar.getJarEntry("plugin.yml");
+						if (entry == null)
 							continue;
-
-						PluginDescription description = null;
-						try {
-							JarFile jar = new JarFile(file);
-							JarEntry entry = jar.getJarEntry("plugin.yml");
-							if (entry == null)
-								continue;
-							InputStream stream = jar.getInputStream(entry);
-							description = new PluginDescription(Utils.readFile(stream));
-						} catch (IOException e) {
-						}
-
-						if (description == null)
-							continue;
-
-						if (description.getName().toLowerCase().equals(pluginName.toLowerCase())) {
-							matchFile = file;
-							break;
-						}
+						InputStream stream = jar.getInputStream(entry);
+						description = new PluginDescription(Utils.readFile(stream));
+					} catch (IOException e) {
 					}
-				}else{
-					matchFile = files;
+
+					if (description == null)
+						continue;
+
+					if (description.getName().toLowerCase().equals(pluginName.toLowerCase())) {
+						matchFile = file;
+						break;
+					}
 				}
 
 				if (matchFile.exists())
@@ -356,6 +356,50 @@ public class YUM {
 						.info("* UPDATED: " + this.pluginName + " [" + this.repoVersion + "]");
 			}
 		}.setData(pluginName, downloadLink, path, repoVersion));
+	}
+
+	public static void UpdateCoreJAR(String downloadLink, String path, String repoVersion) {
+		Server.getInstance().getScheduler().scheduleAsyncTask(new AsyncTask() {
+			private String link, path, repoVersion;
+			private boolean success = false;
+
+			public AsyncTask setData(String link, String path, String repoVersion) {
+				this.link = link;
+				this.path = path;
+				this.repoVersion = repoVersion;
+				return this;
+			}
+
+			@Override
+			public void onRun() {
+				File file = new File(this.path);
+
+				if (file.isDirectory())
+					return;
+
+				if (file.exists())
+					file.delete();
+
+				URL website;
+				try {
+					website = new URL(this.link);
+					try (InputStream in = website.openStream()) {
+						Files.copy(in, file.toPath(), StandardCopyOption.REPLACE_EXISTING);
+						this.success = true;
+					}
+				} catch (Exception e) {
+				}
+			}
+
+			public void onCompletion(Server server) {
+				if (!success) {
+					YUMPlugin.getInstance().getLogger().info("* PASS: NUKKIT CORE #" + this.repoVersion);
+					return;
+				}
+				YUMPlugin.getInstance().getLogger().info("* UPDATED: NUKKIT CORE #" + this.repoVersion);
+				YUM.config.set("coreversion", Integer.valueOf(repoVersion));
+			}
+		}.setData(downloadLink, path, repoVersion));
 	}
 
 	public static void UpdateCore(CommandSender sender) {
@@ -441,10 +485,9 @@ public class YUM {
 					if (matchFile.exists())
 						matchFile.delete();
 
-					YUM.UpdateJAR("NUKKIT CORE",
+					YUM.UpdateCoreJAR(
 							"http://ci.mengcraft.com:8080/job/nukkit/lastSuccessfulBuild/artifact/target/nukkit-1.0-SNAPSHOT.jar",
-							matchFile.getAbsolutePath(), "#" + String.valueOf(nukkitRepoVersion));
-
+							matchFile.getAbsolutePath(), String.valueOf(nukkitRepoVersion));
 					this.isSucces = true;
 				} catch (Exception e) {
 				}
@@ -462,11 +505,6 @@ public class YUM {
 					if (this.message != null)
 						player.sendMessage(this.message);
 				}
-
-				if (!this.isSucces)
-					return;
-
-				YUM.config.set("coreversion", (int) nukkitRepoVersion);
 			}
 		}.setData(senderName, (int) config.get("coreversion")));
 	}
